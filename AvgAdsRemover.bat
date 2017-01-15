97d5c835a0ce4adebfb7b4349c88fe75432eb661
giovi321 <write@giovi.cr>
2017-01-15 14:47:29 +0100

    Update AvgAdsRemover.bat

--- AvgAdsRemover.bat
+++ AvgAdsRemover.bat
@@ -9,8 +9,8 @@ exit
 
 :start
 mkdir "C:\Program Files (x86)\AVG\Av\aaa"
-rmdir "C:\Program Files (x86)\AVG\Av\aaa"
-if exist "C:\Program Files (x86)\AVG\Av\aaa" goto stopavgplease
+if exist "C:\Program Files (x86)\AVG\Av\aaa" goto DeleteAdvFolders
+goto stopavgplease
 
 :DeleteAdvFolders
 rmdir /S /Q "C:\Program Files (x86)\AVG\Av\Awacs"
@@ -24,6 +24,7 @@ pause >nul
 goto start
 
 :eof
+rmdir "C:\Program Files (x86)\AVG\Av\aaa"
 echo Operation completed, you can now start AVG. Press any key to exit.
 pause >nul
 exit
